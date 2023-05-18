fdd046f2e591b01b59f8708c161ab3d67059c9f8
Kevin Taing <ktaing789@gmail.com>
2023-05-18 18:27:03 -0400

    added basic prompts

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'Please provide a name for your project. ',
+    },
+    {
+        type: 'input',
+        name: 'projectDescription',
+        message: 'Please provide a description for your project. ',
+    },
+    {
+        type: 'input',
+        name: 'projectInstallation',
+        message: 'Please provide installation instructions. ',
+    },
+    {
+        type: 'input',
+        name: 'projectUsage',
+        message: 'Please provide usage directions. ',
+    },
+    {
+        type: 'input',
+        name: 'projectLicense',
+        message: 'Please choose a license for your project. ',
+    },
+    {
+        type: 'input',
+        name: 'projectContributer',
+        message: 'Please provide any contributors towards your project. ',
+    },
+    {
+        type: 'input',
+        name: 'projectTesting',
+        message: 'Please provide intructions for testing. ',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
